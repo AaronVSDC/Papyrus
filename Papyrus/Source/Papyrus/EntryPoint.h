c296434ad0c51f54092805557c503b2de2d1c967
Aaron_Van_Sichem_De_Combe <aaronvsdc@gmail.com>
2026-07-22 18:25:06 +0200

    added conditional logging

--- Papyrus/Source/Papyrus/EntryPoint.h
+++ Papyrus/Source/Papyrus/EntryPoint.h
@@ -9,14 +9,6 @@ extern Papyrus::Application* Papyrus::createApplication();
 int main(int, char**) 
 {
 	Papyrus::Log::init(); 
-	PPR_CORE_WARN("Core logger warning"); 
-	PPR_INFO("Client logger info");  
-	PPR_CRITICAL("FATAL ERROR");  
-	PPR_TRACE("tracing something"); 
-	int var = 3; 
-	PPR_CORE_TRACE("var={0}", var); 
-
-
 	auto app = Papyrus::createApplication();
 	app->run();
 	delete app;
